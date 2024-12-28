9d758ac4e9b6465536cb6d14e3126aa749df3546
Youngseo Kang <youngseo.kangg@gmail.com>
2024-12-29 08:57:37 +0900

    feat: rushstack/web-app extends 작성, nano-bundle 적용, pnp -> node_modules 변경

--- packages/eslint/index.js
+++ packages/eslint/index.js
@@ -1,5 +1,9 @@
 const base = require('./src/base');
+const rushstack = require('./src/rushstack');
 
 module.exports = {
-  configs: { base },
+  // base > rushstack 우선순위
+  extends: [...base.extends, ...rushstack.extends],
+  plugins: [...(base.plugins || []), ...(rushstack.plugins || [])],
+  rules: { ...base.rules, ...rushstack.rules },
 };
---
+++ packages/eslint/src/rushstack.js
@@ -0,0 +1,4 @@
+module.exports = {
+  // 브라우저 API와 관련된 rushstack 보안 규칙을 활성화
+  extends: ['@rushstack/eslint-config/profile/web-app'],
+};
